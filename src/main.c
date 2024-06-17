17e99c8319497d864237e954011690b7a21faca1
spooky <spooky@koles.tech>
2024-06-17 19:18:50 +0100

    correct style stuff and whatnot

--- src/main.c
+++ src/main.c
@@ -1,29 +1,25 @@
 #include "raylib.h"
 
-extern void plum( void );
-
-int main(void)
-{
+int main( void ) {
 	const int screenWidth = 800;
-	const int screenHeight = 450;
+	const int screenHeight = 600;
 
-	plum();
-
-	InitWindow(screenWidth, screenHeight, "dismal display of hubris");
-	SetTargetFPS(60);
+	InitWindow( screenWidth, screenHeight, "somatic catastrophe" );
+	SetTargetFPS( 60 );
 
 	Camera2D camera = { 0 };
 	camera.zoom = 1.0f;
 
-	while (!WindowShouldClose())
-	{
-		BeginDrawing();
+	while ( !WindowShouldClose() ) {
+		BeginDrawing()
 		{
 			const Vector2 pos = { 190, 200 };
-			ClearBackground(RAYWHITE);
+			ClearBackground( RAYWHITE );
 
-			BeginMode2D(camera);
-			DrawText("raylib", 190, 200, 20, LIGHTGRAY);
+			BeginMode2D( camera );
+			{
+				DrawText( "i love rock n roll", 190, 200, 20, LIGHTGRAY );
+			}
 			EndMode2D();
 		}
 		EndDrawing();
